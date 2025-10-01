86995dd3ae5fc4bf115c37fcfe7449919ccff4c0
Boredom407 <boredom407@gmail.com>
2025-10-01 20:48:51 +0100

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,23 +3,11 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-      colors: {
-        brandDark: "#2E0700",
-        brandDarker: "#2D0500",
-      },
-      backgroundImage: {
-        "brand-gradient": "linear-gradient(to right, #2E0700, #2D0500)",
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 }
 
